2754bd210b45201a796e0e7a82e75830acc29032
acemaster <vivekhtc25@gmail.com>
2015-08-13 00:13:07 +0530

    New posts route

--- vivek-news/public/javascripts/angularapp.js
+++ vivek-news/public/javascripts/angularapp.js
@@ -10,8 +10,8 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
-    });
-    .state('post',{
+    })
+  .state('post',{
       url: '/post/{id}',
       templateUrl: '/post.html',
       controller: 'PostCtrl'
@@ -40,7 +40,16 @@ app.controller('PostCtrl',[
   '$stateParams',
 'posts',
 function($scope,$stateParams,posts){
-
+   $scope.post = posts.posts[$stateParams.id];
+   $scope.addComment = function(){
+  if($scope.body === '') { return; }
+  $scope.post.comments.push({
+    body: $scope.body,
+    author: 'user',
+    upvotes: 0
+  });
+  $scope.body = '';
+};
 }]);
 
 
@@ -53,7 +62,10 @@ function($scope,posts){
 
 $scope.addpost= function(){
 	if($scope.title || $scope.title!='')
-		$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+		$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0, comments: [
+    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  ]});
 	$scope.title="";
 	$scope.link="";
 }
